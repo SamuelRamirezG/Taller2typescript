c4b87f9762b0dbc428317518772d6e4da14bf4a4
SamuelRamirezG <111009060+SamuelRamirezG@users.noreply.github.com>
2024-04-26 11:09:21 -0500

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,14 +1,22 @@
 import { dataseries } from './dataseries.js';
+var cardText = document.getElementById("info");
 var seriesTbody = document.getElementById("series");
 var temporadapromedio = document.getElementById('temporada-promedio'); // Nodo tbody que tiene el id="courses"
+var showEvent;
 renderSeriesInTable(dataseries);
 temporadapromedio.innerHTML = "".concat(getPromediotemporadas(dataseries));
+function load() {
+    showEvent = document.getElementsByName("click");
+    showEvent.forEach(function (c) { c.onclick = function () { return showSerie(Number(c.id)); }; });
+}
 function renderSeriesInTable(series) {
+    var tbodySerie = document.createElement("tbody");
     series.forEach(function (c) {
-        var trElement = document.createElement("tr");
-        trElement.innerHTML = "<td>".concat(c.numero, "</td>\n                           <td>").concat(c.nombre, "</td>\n                           <td>").concat(c.canal, "</td>\n                           <td>").concat(c.temporadas, "</td>");
-        seriesTbody.appendChild(trElement);
+        tbodySerie.innerHTML +=
+            "<tr name=\"click\" id=".concat(c.numero, ">\n     <td>").concat(c.numero, "</td>\n     <td><a href=\"").concat(c.url, "\"> ").concat(c.nombre, "</a></td> \n     <td>").concat(c.canal, "</td>\n     <td>").concat(c.temporadas, "</td>\n     </tr>\n     ");
+        seriesTbody.appendChild(tbodySerie);
     });
+    load();
 }
 function getPromediotemporadas(series) {
     var suma = 0;
@@ -17,4 +25,12 @@ function getPromediotemporadas(series) {
     series.forEach(function (serie) { return suma = suma + serie.temporadas; }, totalseries = series.length);
     promedio = Math.round(suma / totalseries);
     return promedio;
+}
+function showSerie(number) {
+    console.log("asdibasda");
+    cardText.childNodes.forEach(function (c) { cardText.removeChild(c); });
+    var serie = dataseries[number - 1];
+    var cardBody = document.createElement("div");
+    cardBody.innerHTML = "\n  <img class=\"card-img-top\" src=".concat(serie.imagen, " alt=\"Card image\">\n  <div class=\"card-body\">\n      <h4 class=\"card-title\">").concat(serie.nombre, "</h4>\n      <p class=\"card-text\">").concat(serie.resenia, "</p>\n      <a href=\"").concat(serie.url, "\" >").concat(serie.url, "</a>\n  </div>    \n  ");
+    cardText.appendChild(cardBody);
 }
