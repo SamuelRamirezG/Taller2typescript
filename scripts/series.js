f1a67b7f0a70d38fb04b2382fc87d2241e1b69c8
SamuelRamirezG <111009060+SamuelRamirezG@users.noreply.github.com>
2024-04-26 11:09:42 -0500

    Update series.js

--- scripts/series.js
+++ scripts/series.js
@@ -1,9 +1,12 @@
 var series = /** @class */ (function () {
-    function series(numero, nombre, canal, temporadas) {
+    function series(numero, nombre, canal, temporadas, resenia, imagen, url) {
         this.numero = numero;
         this.nombre = nombre;
         this.canal = canal;
         this.temporadas = temporadas;
+        this.resenia = resenia;
+        this.imagen = imagen;
+        this.url = url;
     }
     return series;
 }());
